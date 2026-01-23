6597022515eb95ac92465cd57e0aa28430a7a8d8
Walid Yachou <Walid.Yachou@devops.ncia.nato.int>
2026-01-23 15:14:08 +0100

    add nvim config

---
+++ private_dot_config/private_nvim/private_lua/private_custom/private_plugins/init.lua
@@ -0,0 +1,5 @@
+--github/copilot.vim You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {}
