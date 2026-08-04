82b08b46c97af11d664d641be40dcff16c8bcc99
Walid Yachou <Walid.Yachou@devops.ncia.nato.int>
2026-08-04 13:28:22 +0200

    nvim: focus undotree window when toggled

--- private_dot_config/private_nvim/private_lua/private_custom/private_plugins/undotree.lua
+++ private_dot_config/private_nvim/private_lua/private_custom/private_plugins/undotree.lua
@@ -1,6 +1,10 @@
 return {
   'mbbill/undotree',
 
+  init = function()
+    vim.g.undotree_SetFocusWhenToggle = 1
+  end,
+
   config = function()
     vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = 'Toggle [U]ndotree' })
   end,
